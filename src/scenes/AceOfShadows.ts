cadb7791b8135cb41967ede17f6cdf8dd6c5c52b
Adi <adriandumitrascu87@gmail.com>
2026-02-04 14:21:07 +0200

    Added unit containers

---
+++ src/scenes/AceOfShadows.ts
@@ -0,0 +1,14 @@
+import { Container } from "pixi.js";
+import type { IUnit } from "../utils/IUnit";
+
+export class AceOfShadows extends Container implements IUnit {
+
+  constructor() {
+    super();
+
+  }
+
+  onEnterUnit(): void {}
+
+  onExitUnit(): void {}
+}
---
+++ src/scenes/PhoenixFlame.ts
@@ -0,0 +1,14 @@
+import { Container } from "pixi.js";
+import type { IUnit } from "../utils/IUnit";
+
+export class PhoenixFlame extends Container implements IUnit {
+
+  constructor() {
+    super();
+
+  }
+
+  onEnterUnit(): void {}
+
+  onExitUnit(): void {}
+}
