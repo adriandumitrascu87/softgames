2411b049affe93efc069e20b7b547b14f4a95e28
Adi <adriandumitrascu87@gmail.com>
2026-02-06 12:28:35 +0200

    simplify main.ts using GameInit

---
+++ src/game/GameInit.ts
@@ -0,0 +1,46 @@
+import { Container, type Application } from "pixi.js";
+import { LevelManager } from "./LevelManager";
+import { FpsCounter } from "../utils/FpsCounter";
+import { LevelLoader } from "./LevelLoader";
+import { TitleScreen } from "../scenes/TitleScreen";
+
+export class GameInit {
+  private app: Application;
+
+  private gameLayer = new Container();
+  private uiLayer = new Container();
+
+  private levelManager: LevelManager;
+  private fpsCounter: FpsCounter;
+
+  constructor(app: Application) {
+    this.app = app;
+
+    this.app.stage.addChild(this.gameLayer);
+    this.app.stage.addChild(this.uiLayer);
+
+    this.levelManager = new LevelManager(this.app.stage);
+    this.fpsCounter = new FpsCounter();
+
+    this.uiLayer.addChild(this.fpsCounter);
+
+    this.initLevels();
+    this.iniLoop();
+  }
+
+  initLevels = () => {
+    const loader = new LevelLoader(this.levelManager);
+    loader.init();
+
+    this.levelManager.loadLevel(new TitleScreen());
+  };
+
+  iniLoop = () => {
+    this.app.ticker.add((ticker) => {
+      this.fpsCounter.update(ticker.deltaMS);
+
+      this.levelManager.onResize(window.innerWidth, window.innerHeight);
+      this.fpsCounter.resize();
+    });
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,17 @@
-import { Application, Container } from "pixi.js";
-import { FpsCounter } from "./utils/FpsCounter";
-import { TitleScreen } from "./scenes/TitleScreen";
-import { LevelManager } from "./scenes/LevelManager";
-import { LevelLoader } from "./scenes/LevelLoader";
+import { Application } from "pixi.js";
+import { GameInit } from "./game/GameInit";
+
 
 const app = new Application();
-const fpsCounter = new FpsCounter(5, 5);
 
-const debugLayer = new Container();
 
 await app.init({
   resizeTo: window,
   backgroundColor: 0x1e1e1e,
 });
 
+
 document.body.appendChild(app.canvas);
 
-const levelManager = new LevelManager(app.stage);
-const levelLoader = new LevelLoader(levelManager);
-levelLoader.init();
+new GameInit(app);
 
-//load first level - Title Screen
-levelManager.loadLevel(new TitleScreen());
-
-app.stage.addChild(debugLayer);
-debugLayer.addChild(fpsCounter);
-
-app.ticker.add((ticker) => {
-  fpsCounter.update(ticker.deltaMS);
-});
-
-window.addEventListener("resize", () => {
-  app.renderer.resize(window.innerWidth, window.innerHeight);
-  levelManager.onResize(app.screen.width, app.screen.height);
-  fpsCounter.resize();
-});
