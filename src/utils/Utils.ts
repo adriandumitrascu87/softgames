06a48dbe0cd47392511cb8c89d400182c4346f08
Adi <adriandumitrascu87@gmail.com>
2026-02-07 23:08:31 +0200

    added Utils clans and migrated comon methods

---
+++ src/utils/Utils.ts
@@ -0,0 +1,85 @@
+import { Container } from "pixi.js";
+import { gsap } from "gsap";
+import type { UIButton } from "../ui/UIButton";
+
+export class Utils {
+  static recursiveKillTweens(container: Container) {
+    // Kill tweens on the container itself
+    gsap.killTweensOf(container);
+
+    // Kill tweens recursively for children
+    container.children.forEach((child) => {
+      if (child instanceof Container) {
+        Utils.recursiveKillTweens(child);
+      } else {
+        gsap.killTweensOf(child);
+      }
+    });
+  }
+
+  // creates spiral positons for an array of objects
+  static getSpiralPosition(
+    index: number,
+    angleStep: number = 0.1,
+    radiusStep: number = 0.4,
+  ) {
+    const angle = index * angleStep;
+    const radius = index * radiusStep;
+
+    return {
+      x: Math.cos(angle) * radius,
+      y: Math.sin(angle) * radius,
+    };
+  }
+
+  // Generate a random point inside a circle
+  static randomPointInCircle(radius: number) {
+    const t = Math.random() * Math.PI * 2;
+    const r = Math.sqrt(Math.random()) * radius;
+
+    return {
+      x: Math.cos(t) * r,
+      y: Math.sin(t) * r,
+    };
+  }
+
+    // tween a object to the a destination position
+  static tweenContainerTo(cont: Container, destX: number, destY: number, duration:number) {
+    gsap.to(cont, {
+      x: destX,
+      y: destY,
+      duration: duration, // animation lasts 2 seconds
+      angle: 360,
+      ease: "power2.inOut",
+    });
+  }
+
+  // Toggle a button' s visibility and input state with animation
+  static toggleButtonVisibility(
+    button: UIButton,
+    enable: boolean,
+    delay: number = 0,
+  ) {
+    if (enable) {
+      button.scale.set(0, 0);
+      button.visible = true;
+    } else {
+      button.disableInput();
+    }
+
+    const newScale = enable ? 1 : 0;
+    const ease = enable ? "power1.out" : "power1.in";
+
+    gsap.to(button.scale, {
+      x: newScale,
+      y: newScale,
+      duration: 0.1,
+      ease: ease,
+      delay: delay,
+      onComplete: () => {
+        button.visible = enable;
+        if (enable) button.enableInput();
+      },
+    });
+  }
+}
